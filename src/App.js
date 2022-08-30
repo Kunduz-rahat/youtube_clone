c1b64e45b36d5169dc2ac529ed7d9260850fc88e
Kunduz Rahat <rahatova.kunduz@gmail.com>
2022-08-30 22:27:32 +0600

    add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home';
+import {Box} from '@mui/material';
+import VideoDetail from './pages/VideoDetail';
+import ChannelDetail from './pages/ChannelDetail';
+import Search from './pages/Search';
 
 function App() {
   return (
-    <div className="App">
+  <BrowserRouter>
+  <Box sx={{backgroundColor:"#000"}}>
+  <Routes>
+      <Route path='/' exact element={<Home/>}/>
+      <Route path='/video/:id' element={<VideoDetail/>}/>
+      <Route path='/channel/:id' element={<ChannelDetail/>}/>
+      <Route path='/search/:searchTeam' element={<Search/>}/>
+    </Routes>
+  </Box>
     
-    </div>
+  </BrowserRouter>
   );
 }
 
