4c9c4ab172b5804462a4e0d4baa0e8b31a450fc1
Kunduz Rahat <rahatova.kunduz@gmail.com>
2022-09-01 17:16:01 +0600

    add react player

--- src/pages/VideoDetail/index.jsx
+++ src/pages/VideoDetail/index.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { Typography, Box, Stack } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+
+import Videos from '../../components/Videos'
+import { fetchFromAPI } from "../../utils/fetchFromApi";
+import Loader from '../../components/Loader';
 
 export default function VideoDetail() {
-  return (
-	 <div>VideoDetail</div>
-  )
+  const {id} = useParams();
+  const [videoDetail, setVideoDetail] = useState(null)
+
+  useEffect(()=>{
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
+    .then((data)=>setVideoDetail(data.items[0]))
+  }, [id])
+ return(
+  <Box minHeight="95vh">
+    <Stack direction={{xs:"column", md:"row"}}>
+      <Box flex={1}>
+        <Box sx={{width:"100%", position:"sticky", top:"86px"}}>
+          <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`}/>
+        </Box>
+      </Box>
+    </Stack>
+  </Box>
+ )
+
 }
